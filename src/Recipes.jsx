03f8fd7d8c6ea45ee550ef0ec31f4a57f23c4500
Linton Mackereth <linton@Lintons-MBP.home>
2017-09-13 20:03:37 +0100

    initial file structure setup

---
+++ src/Recipes.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Recipes.css'
+
+class Recipes extends Component {
+  render () {
+    return (
+      <div className='recipes-container' />
+    )
+  }
+}
+
+export default Recipes;
